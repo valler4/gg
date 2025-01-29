2f5de059bb089ee3abc61832a4f9d2ca9f36b988
valler4 <valler1270@gmail.com>
2025-01-29 03:54:40 +0200

    x

--- index.php
+++ index.php
@@ -1,63 +1,16 @@
 <?php
-$a = "kg";
-$b = 100000;
-$d = "@gmail.com";
-$excludedDigits = array(8, 6, 1, 0);
-$uniqueEmails = array();
+  date_default_timezone_set("Africa/Cairo");
 
-while ($b <= 999999) {
-  $email = "$a$b$d";
-  $isValid = true;
+  $reg = date_create("2008-01-01");
 
-  $emailDigits = array();
-  foreach (str_split($b) as $digit) {
-    if (!in_array($digit, $emailDigits)) {
-      $emailDigits[] = $digit;
-    } else {
-      $isValid = false;
-      break;
-    }
-  }
- 
-  
+echo '<pre>';
+print_r(getdate());
+echo '<pre>';
 
-  if ($isValid) {
-    $hasDigits = true;
-    foreach (array(3, 4) as $digit) {
-      if (!in_array($digit, $emailDigits)) {
-        $hasDigits = false;
-        break;
-      }
-    }
-    if ($hasDigits) {
-      if ($emailDigits[0] == 3 && $emailDigits[1] == 4) {
-        foreach ($excludedDigits as $digit) {
-          if (strpos($email, (string)$digit) !== false) {
-            $isValid = false;
-            break;
-          }
-        }
-        if ($isValid) {
-          $uniqueEmails[] = $email;
-        }
-      }
-    }
-  }
+echo time(). "<br>";
 
-  $b++;
-}
-
-foreach ($uniqueEmails as $email) {
-  echo "$email" . "<br>";
-}
-echo "<hr>";
-?>
-<?php
-$filename = "h.php";
-$text = file_get_contents($filename);
-$lines = explode("\n", $text);
-
-foreach ($lines as $line) {
-    echo $line . "<br>";
-}
+echo time() / 60 . "<br>";
+echo time() / 60 / 60 . "<br>";
+echo time() / 60 / 60 / 24 . "<br>";
+echo time() / 60 / 60 / 24 / 365 . "<br>";
 ?>
